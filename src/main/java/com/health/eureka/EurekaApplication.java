d46b062866b7322b9be85b985f474e389799bccf
erashape <erashape@gmail.com>
2022-03-20 08:10:31 +0900

    Eureka

--- src/main/java/com/health/eureka/EurekaApplication.java
+++ src/main/java/com/health/eureka/EurekaApplication.java
@@ -2,10 +2,11 @@ package com.health.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
